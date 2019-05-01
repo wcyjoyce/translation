f796f571bf153fa0d2d6a9f601adeb980c9231e5
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-05-01 09:11:10 +0800

    initial commit with boilerplate

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/app.jsx";
+import "../assets/stylesheets/application.scss";
+
+ReactDOM.render(
+  <div className="app"><App /></div>,
+  document.getElementById("root")
+);
